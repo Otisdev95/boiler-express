e7f17bb234e3ed52ff8024b6768c0e833f05b1aa
otisdev95 <olaliotis95@gmail.com>
2023-10-21 04:24:23 +0100

    updated boiler-express

--- myApp.js
+++ myApp.js
@@ -3,12 +3,16 @@ let app = express();
 
 console.log("Hello World");
 
+app.get("/", (req, res) => {
+    res.send('Hello Express');
+});
+
 // app.get("/", (req, res) => {
-//     res.send('Hello Express');
+//     res.sendFile(absolutePath = __dirname + '/views/index.html');
 // });
 
-app.get("/", (req, res) => {
-    res.sendFile(absolutePath = __dirname + 'views/index.html');
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 
