37f27b318f43c1ecc3b25139188824b04f61b744
otisdev95 <olaliotis95@gmail.com>
2023-10-21 05:24:41 +0100

    updated boiler-express

--- myApp.js
+++ myApp.js
@@ -5,7 +5,11 @@ app.get("/", function (req, res) {
     res.sendFile(__dirname + '/views/index.html');
 });
 
-app.use("/public", express.static(__dirname + '/public'));
+app.use("/public", express.static(__dirname + '/public/'));
+
+app.get("/json", (req, res) => {
+    res.json({"message": "Hello json"});
+});
 
 
 
