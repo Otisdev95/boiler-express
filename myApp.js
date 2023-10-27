d21cb1a777511eddc03a933cd11a951e38cfe9f2
otisdev95 <olaliotis95@gmail.com>
2023-10-27 13:07:24 +0100

    updated boiler-express

--- myApp.js
+++ myApp.js
@@ -10,7 +10,7 @@ app.use("/public", express.static(__dirname + '/public/'));
 
 app.get("/json", (req, res) => {
     if (process.env.MESSAGE_STYLE == "uppercase") {
-        res.json({"message": "Hello json".toUpperCase()});
+        res.json({"message": "HELLO JSON"});
     } else {
         res.json({"message": "Hello json"});
     }
