744d40d4e4e4c816b0aa6d90424dcadc784a7b04
otisdev95 <olaliotis95@gmail.com>
2023-11-09 12:54:57 +0100

    updated boiler-express

--- myApp.js
+++ myApp.js
@@ -23,6 +23,13 @@ app.get("/json", (req, res) => {
     }
 });
 
+app.get("/now", (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+}, (req, res) => {
+    res.send(req.time);
+});
+
 
 
 
